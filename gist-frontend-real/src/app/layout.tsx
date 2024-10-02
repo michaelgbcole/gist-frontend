433cd50c42ad265874605c6129a40a4612ebe568
Michael <mgbcsw13@gmail.com>
2024-10-02 13:32:28 -0700

    add posthog

--- gist-frontend-real/src/app/layout.tsx
+++ gist-frontend-real/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { CSPostHogProvider } from './providers'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <CSPostHogProvider>
       <body className={inter.className}>{children}
         <Analytics />
         <SpeedInsights />
       </body>
+      </CSPostHogProvider>
     </html>
   );
 }
